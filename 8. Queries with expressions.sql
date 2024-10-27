ea3297143798393c560b4132f1ce0abb7edf5cbc
ristespasov <ristespasov@hotmail.com>
2024-10-27 18:21:08 +0100

    sql: queries with expressions

--- 8. Queries with expressions.sql
+++ 8. Queries with expressions.sql
@@ -40,19 +40,17 @@ Table: boxoffice
 */
 
 
-/* 1. Find the domestic and international sales for each movie */
-SELECT title, domestic_sales, international_sales
-FROM boxoffice
-INNER JOIN movies ON boxoffice.movie_id = movies.id
+/* 1. List all movies and their combined sales in millions of dollars */
+SELECT title, (domestic_sales + international_sales) / 1000000 AS gross_sales_millions
+FROM movies
+JOIN boxoffice ON movies.id = boxoffice.movie_id;
 
-/* 2. Show the sales numbers for each movie that did better internationally rather than domestically */
-SELECT title, domestic_sales, international_sales
-FROM boxoffice
-INNER JOIN movies ON boxoffice.movie_id = movies.id
-WHERE international_sales > domestic_sales
+/* 2. List all movies and their ratings in percent */
+SELECT title, rating * 10 AS rating_percent
+FROM movies
+JOIN boxoffice ON movies.id = boxoffice.movie_id;
 
-/* 3. List all the movies by their ratings in descending order */
-SELECT title, rating
-FROM boxoffice
-INNER JOIN movies ON boxoffice.movie_id = movies.id
-ORDER BY rating DESC
+/* 3. List all movies that were released on even number years */
+SELECT title, year
+FROM movies
+WHERE year % 2 = 0
