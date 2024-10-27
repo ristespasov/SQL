963a545169e73407aee6aa6abc0e1710d639b49e
ristespasov <ristespasov@hotmail.com>
2024-10-27 13:51:02 +0100

    sql: filtering data with null values

--- 7. Filtering NULL values.sql
+++ 7. Filtering NULL values.sql
@@ -31,14 +31,13 @@ Table: employees
 */
 
 
-/* 1. Find the list of all buildings that have employees */
-SELECT DISTINCT building
+/* 1. Find the name and role of all employees who have not been assigned to a building */
+SELECT  name, role
 FROM employees
+WHERE building IS NULL
 
-/* 2. Find the list of all buildings and their capacity */
-SELECT * FROM buildings
-
-/* 3. List all buildings and the distinct employee roles in each building (including empty buildings) */
-SELECT DISTINCT building_name, role
+/* 2. Find the names of the buildings that hold no employees */
+SELECT DISTINCT building_name
 FROM buildings
 LEFT JOIN employees ON building_name = building
+WHERE role IS NULL;
