2df97ef7b531d84c03a3eb1ccc6ae198bd8e8c17
ristespasov <ristespasov@hotmail.com>
2024-10-27 13:20:28 +0100

    sql: outer joins

--- 6. Outer JOINs.sql
+++ 6. Outer JOINs.sql
@@ -33,9 +33,9 @@ SELECT DISTINCT building
 FROM employees
 
 /* 2. Find the list of all buildings and their capacity */
-SELECT * FROM buildings;
+SELECT * FROM buildings
 
 /* 3. List all buildings and the distinct employee roles in each building (including empty buildings) */
 SELECT DISTINCT building_name, role
 FROM buildings
-LEFT JOIN employees ON building_name = building;
+LEFT JOIN employees ON building_name = building
